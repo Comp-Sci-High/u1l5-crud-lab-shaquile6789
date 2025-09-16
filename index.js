5b556aa1faf140d6c154ea5de8579a28d37571d5
shaquile6789 <shaquille.oemrawsingh26@compscihigh.org>
2025-09-16 19:16:29 +0000

    Competes up to Task 4

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // ARRAY SECTION
 // Task 1: Create an empty array called playlist
-
+let playlist =[];
 
 // Task 2: Push 3 songs to the playlist array
-
+playlist.push("No introduction");
+playlist.push("Gods Plan");
+playlist.push("wgft");
 
 // Task 3: Update the value of the last element to be "Test"
-
+playlist[playlist.length-1] = "Test";
 
 // Task 4: Delete the "Test" element 
-
+playlist.pop;
 // Console log the array
-
+console.log(playlist);
 
 // Commit your changes with an appropriate message.
 // ----------------------------------
